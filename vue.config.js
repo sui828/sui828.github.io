203e263d1c1383b7e59fb8ffcbaa7650065a4472
sui828 <hasuya21sui@gmail.com>
2023-11-14 21:26:20 +0900

    delete dist

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: './',
+  outputDir: "docs",
   transpileDependencies: true,
 
   chainWebpack: config => {
